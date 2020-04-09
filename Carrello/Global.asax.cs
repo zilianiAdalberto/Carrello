b5c2a981dc252d6dc7c813f27363b8c73571aa19
Alberto Ziliani <ziliani.alberto@outlook.it>
2020-04-09 14:07:41 +0200

    test

--- Carrello/Global.asax.cs
+++ Carrello/Global.asax.cs
@@ -16,6 +16,6 @@ namespace Carrello
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
+        } 
     }
 }
